a86f6b721d116291e485eae6c0e7b8c20293cb93
Данила Щелков (Danila Schelkov) <54549682+Vorono4ka@users.noreply.github.com>
2021-04-02 23:18:11 +0300

    Structure update
    Now commands are added by decorators

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from vk_api.longpoll import VkEventType, VkLongPoll
-import vk_api
+import json
 
-from commands.factory import CommandsFactory
-from commands.receive.Fix import Fix
-from commands.receive.Hello import Hello
-from utils.errors import Errors
+from vk_api import *
+from vk_api.keyboard import *
+from vk_api.longpoll import VkEventType, VkLongPoll
+
+commands = []
+
+
+def command(command_name, **kwargs):
+    aliases = kwargs['aliases'] if 'aliases' in kwargs else []
+    access_level = kwargs['access_level'] if 'access_level' in kwargs else 0
+
+    def wrapper(function):
+        commands.append((function, command_name, aliases, access_level))
+
+    return wrapper
 
 
 def _(*args):
@@ -14,69 +24,79 @@ def _(*args):
     print()
 
 
-def calculate_file_size(size_in_bytes):
-    suffix = 'Bytes'
-    size = size_in_bytes
-    if size_in_bytes > 1024:
-        suffix = 'Kb'
-        size /= 1024
-        if size_in_bytes > 1024 * 1024:
-            suffix = 'Mb'
-            size /= 1024
-
-    return f'{int(round(size, 0))}{suffix}'
-
-
 class VkBot:
-    def __init__(self, token: str):
-        self.commands_factory = CommandsFactory()
-        self.errors = Errors()
-
+    def __init__(self, token: str, group_id: int = None):
         vk_session = vk_api.VkApi(token=token)
 
-        self.longpoll = VkLongPoll(vk_session)
+        self.longpoll = VkLongPoll(vk_session, group_id=group_id)
         self.vk = vk_session.get_api()
 
     def listen(self):
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW:
-                msg_id = event.message_id
-                p_id = event.peer_id
-                msg = self.vk.messages.getById(message_ids=msg_id,
-                                               peer_id=p_id)['items'][0]
+                message_id = event.message_id
+                peer_id = event.peer_id
+                message = self.vk.messages.getById(message_ids=message_id, peer_id=peer_id)['items'][0]
 
-                text = msg['text']
-                f_id = msg['from_id']
+                from_id = message['from_id']
+                text = message['text']
 
-                cmd_payload = None
+                if 'payload' in message:
+                    payload = json.loads(message['payload'])
+
+                payload = None
                 cmd = None
 
                 for prefix in ['!!', '/', '.']:
                     if text.startswith(prefix):
                         msg_without_prefix = text[len(prefix):]
                         if len(msg_without_prefix) > 0:
-                            cmd = msg_without_prefix.split()[0]
-                            cmd_payload = msg_without_prefix[len(cmd) + 1:]
-                            if len(cmd_payload) == 0:
-                                cmd_payload = None
+                            cmd = msg_without_prefix.split()[0].lower()
+                            payload = msg_without_prefix[len(cmd) + 1:] or None
                         break
 
                 if cmd is not None:
-                    executed, error_code = self.commands_factory.handle_command(self.vk, cmd, cmd_payload, 0, msg)
-                    if not executed:
-                        if p_id == 564750551:
+                    access_level = 0
+
+                    if from_id == 356219576:
+                        access_level = 4
+
+                    try:
+                        for executor, name, aliases, required in commands:
+                            if access_level < required:
+                                self.vk.messages.send(
+                                    peer_id=peer_id,
+                                    message='Ваш уровень доступа меньше нужного.',
+                                    random_id=0
+                                )
+                                continue
+                            elif not (cmd in aliases or cmd == name):
+                                continue
+                            executor(self, message, payload, access_level)
+                            _('Command:', cmd, 'Command Payload:', payload)
+                    except Exception as e:
+                        if from_id == 356219576:
                             self.vk.messages.send(
-                                peer_id=p_id,
-                                message=self.errors.localize_error(error_code, 'ru-RU'),
+                                peer_id=356219576,
+                                message=e,
                                 random_id=0
                             )
-                    _('Command:', cmd, 'Command Payload:', cmd_payload)
+
+    @command('help', aliases=['помощь'])
+    def help(self, message: dict, payload, access_level):
+        text = ''
+        for _, name, aliases, command_access_level in commands:
+            if access_level < command_access_level:
+                continue
+            
+            text += f'{name.upper()}'
+            if aliases:
+                text += f': {", ".join(aliases)}'
+            text += '\n'
+
+        self.vk.messages.send(peer_id=message['peer_id'], message=text, random_id=0)
 
 
 if __name__ == '__main__':
-    bot = VkBot('172da3a0dae9f99545bb712af4bf34faedbcb4f13dadfa10fb1327106d82aecd5c7e5499e71d6c4fa5a70')
-
-    # bot.commands_factory.add_command(['hello', 'test', 'привет', 'тест'], Hello)
-    bot.commands_factory.add_command(['fix', 'фикс'], Fix)
-
+    bot = VkBot('TOKEN')
     bot.listen()
